695f925e562c599d1af4384adba42d34ae646dda
Git-jayam55 <98252793+jayampatel55@users.noreply.github.com>
2024-10-24 09:49:22 +0530

    Update q1.c

--- ass1/q1.c
+++ ass1/q1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("Name: Steve Mathew\n");
+    printf("Name: Jayam Patel\n");
     printf("Address: 1234 Pochinki\n");
     printf("Birth Date: January 1, 1990\n");
     return 0;
